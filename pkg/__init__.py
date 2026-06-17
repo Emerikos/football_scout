96054a411f4a4d93e8eff8e61f5dc557024f3f52
Godwin Nwachukwu <Godwinnwachukwu7@gmail.com>
2026-06-17 12:07:39 +0100

    fix circular import

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -6,20 +6,22 @@ from flask_wtf import CSRFProtect
 csrf = CSRFProtect()
 def create_app():
     from pkg.models import db
-    from pkg import config
+    from pkg.config import Config
     
     app = Flask(__name__,instance_relative_config=True)
     app.config.from_pyfile('config.py')
-    app.config.from_object(config.Config)
+    app.config.from_object(Config)
 
     db.init_app(app)
     migrate = Migrate(app,db)
     csrf.init_app(app)
+    from pkg import user_routes,admin_routes,models,forms
+    
     return app
 
 app = create_app()
 
-from pkg import user_routes,admin_routes,models,forms
+
 
 
 
