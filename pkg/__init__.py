9c13014ef563687d55a42c4dcbc000f78b727990
Godwin Nwachukwu <Godwinnwachukwu7@gmail.com>
2026-06-17 12:30:06 +0100

    __init__ reset

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, app
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
 
@@ -9,17 +9,20 @@ def create_app():
     from pkg import config
     
     app = Flask(__name__,instance_relative_config=True)
-    app.config.from_pyfile('config.py')
-    app.config.from_object(config.Config)
+    # app.config.from_pyfile('config.py')
+    # app.config.from_object(config.Config)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
     migrate = Migrate(app,db)
     csrf.init_app(app)
+    from pkg import user_routes,admin_routes,models,forms
     return app
 
 app = create_app()
 
-from pkg import user_routes,admin_routes,models,forms
+
 
 
 
